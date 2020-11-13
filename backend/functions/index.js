94350b881dcf1611b4a902a09e139b6a3817cee2
utopy <utopy.core@gmail.com>
2020-11-13 17:30:50 +0100

    First beautiful commit

---
+++ backend/functions/index.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors')
+const pronote = require('pronote-api');
+
+app.use(cors());
+
+app.get('/pronote/lastmessage', async (req, res) => {
+  const url = req.query.url;
+  const username = req.query.username;
+  const password = req.query.password;
+
+  const login = await pronote.login(url, username, password/*, cas*/);
+  const infos = await (await login.infos()).slice(0, 4);
+  let infosSorted = infos.sort(function(a,b){
+    return new Date(b.date) - new Date(a.date);
+  });
+  res.send(infosSorted)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+exports.app = functions.https.onRequest(app);
